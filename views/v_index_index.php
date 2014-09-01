4acaf72d1e0391c6ab26dea6ea9208dbc53225d8
ann adelsberger <adelsbergerann@gmail.com>
2014-09-01 10:47:55 -0400

    update holder page

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,11 +1,6 @@
-<p>
-	Hello World! You have successfully spawned a new application.
-</p>
+<p><strong>
+	Welcome to 02143.somervilleunion.net <br>
+	Stay tuned for updates!
+</strong></p>
 
-<p>
-	This message is being triggered via the c_index.php controller, within the index() method.
-</p>
-
-<p>
-	<strong>Since everything is in working order, you should now delete <?php echo APP_PATH?>diagnostics.php</strong>
-</p>
+ 
