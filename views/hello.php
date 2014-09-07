95e2d76bfd59666ac9d4fb936fe6e84aacfa1579
ann adelsberger <adelsbergerann@gmail.com>
2014-09-07 14:47:02 -0400

    add test doc

---
+++ views/hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+
+<?php 
+echo "<h3>This is is a test.....</h3>"; 
+echo "<h4>This is is a only a test....</h4>"; 
+echo "Hello World!!!!"; 
+?> 
+
+</body> 
+</html>
